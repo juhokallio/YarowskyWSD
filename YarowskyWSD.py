ee7ddedb426be21ccaad13f10462e99e25e08c5c
Otto H Wallenius <ottowall@dat-7.cs.helsinki.fi>
2014-11-07 18:56:59 +0200

    Refactor collocation logic to collogation class, create probability calculations

--- YarowskyWSD.py
+++ YarowskyWSD.py
@@ -1,6 +1,46 @@
 #coding: utf8
 
 import unittest
+import math
+
+
+class Collocation:
+    # Epsilon
+    E = 0.1
+
+    def __init__(self, words, rule, sense_count):
+        self.words = words
+        self.rule = rule
+        self.senses = [0] * sense_count
+        self.count = 1
+
+    # Probability
+    def p(self, sense):
+        if sense in self.senses:
+            return (self.senses[sense] + self.E) / (self.count + self.E * len(self.senses))
+        else:
+            raise Exception("No such sense")
+
+    def log_likelihood(self, sense):
+        p_sense = self.p(sense)
+        return math.log(p_sense / (1 - p_sense), 2)
+
+    def plus(self, sense):
+        if sense in self.senses:
+            self.senses[sense] += 1
+        else:
+            self.senses[sense] = 1
+        self.count += 1
+        return self
+
+    def get_sense_count(self, sense):
+        return self.senses[sense]
+
+    def __hash__(self):
+        return hash((self.words, self.rule))
+
+    def __eq__(self, other):
+        return (self.words, self.rule) == (other.words, other.rule)
 
 
 def init_context_list(contexts, seeds):
@@ -19,15 +59,12 @@ def build_collocations(contexts_with_senses, pattern, k):
     # 0: Word immediately to the right
     # 1:
     #
-    collocations = [{}, {}, {}, {}, {}, {}]
+    collocations = {}
 
-    def add_collocation(c_index, word, sense):
-        # Not yet in collocation
-        if word not in collocations[c_index]:
-            collocations[c_index][word] = {}
-            collocations[c_index][word][sense] = 1
-        else:
-            collocations[c_index][word][sense] += 1
+    def add_collocation(words, rule, sense):
+        if (words, rule) not in collocations:
+            collocations[(words, rule)] = Collocation(words, rule, sense)
+        collocations[(words, rule)].plus(sense)
 
     for context_with_sense in contexts_with_senses:
         context = context_with_sense[0]
@@ -37,16 +74,25 @@ def build_collocations(contexts_with_senses, pattern, k):
         index = index_of_pattern(context, pattern, k)
         for word_index, word in enumerate(context):
             if abs(word_index - index) > 1:
-                add_collocation(2, word, sense)
+                add_collocation(word, 2, sense)
             elif word_index == index + 1:
-                add_collocation(0, word, sense)
+                add_collocation(word, 0, sense)
             elif word_index == index - 1:
-                add_collocation(1, word, sense)
+                add_collocation(word, 1, sense)
 
             if word_index == index - 2:
                 word_pair = (word, context[word_index + 1])
-                add_collocation(3, word_pair, sense)
+                add_collocation(word_pair, 3, sense)
     return collocations
+
+
+# TODO: optimize with rbtree or something more sensible
+def build_collocation_likelihoods(collocations):
+     epsilon = 0.1
+   #  for collocation in collocations:
+
+
+
 
 # Currently doesn't take account repeating patterns in the end or beginning of corpus.
 # Flag based approach should fix this, but gains are minimal.
@@ -108,11 +154,21 @@ class TextExtractionTest(unittest.TestCase):
         self.assertEqual(context_list[0].count(pattern), 2)
         context_list = init_context_list(context_list, ["yleisiä"])
         collocations = build_collocations(context_list, pattern, k)
-        self.assertEqual(collocations[0]["opit"][0], 1)
-        self.assertEqual(collocations[0]["menetelmiä"][0], 1)
-        self.assertEqual(collocations[2]["tämän"][0], 1)
-        self.assertEqual(collocations[2]["hetken"][0], 1)
-        self.assertTrue("tutustumaan" not in collocations[2])
-        self.assertTrue("tämän" not in collocations[1])
-        self.assertEqual(collocations[3][("tutkimusongelmiin", "molekyylibiologiassa")][0], 1)
+        self.assertEqual(collocations["opit", 0].get_sense_count(0), 1)
+        self.assertEqual(collocations["menetelmiä", 0].get_sense_count(0), 1)
+        self.assertEqual(collocations["tämän", 2].get_sense_count(0), 1)
+        self.assertEqual(collocations["hetken", 2].get_sense_count(0), 1)
+        self.assertTrue(("tutustumaan", 2) not in collocations)
+        self.assertTrue(("tämän", 1) not in collocations)
+        self.assertEqual(collocations[(("tutkimusongelmiin", "molekyylibiologiassa"), 3)].get_sense_count(0), 1)
 
+    def test_p(self):
+        collocation = Collocation("a", 0, 0)
+        self.assertEqual(collocation.p(0), 1)
+        self.assertEqual(collocation.p(1), 0)
+        collocation.plus(0).plus(1).plus(2)
+        self.assertAlmostEqual(collocation.p(0), 0.48, delta=0.01)
+
+    def test_log_likelihood(self):
+        collocation = Collocation("a", 0, 0)
+        self.assertEqual(collocation.log_likelihood(0), 0)
